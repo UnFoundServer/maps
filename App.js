c759b43c23f9144aadf94a9c08261142578f351e
Олег <48626941+UnFoundServer@users.noreply.github.com>
2024-06-17 03:25:56 +0500

    dsa

--- App.js
+++ App.js
@@ -9,8 +9,6 @@ function init() {
         controls: ['zoomControl', 'searchControl', 'trafficControl']
     });
 
-    var multiRoute;
-
     // Function to build the route
     window.buildRoute = function() {
         var startAddress = document.getElementById('start').value;
@@ -23,38 +21,40 @@ function init() {
             ymaps.geocode(finishAddress, { results: 1 }).then(function (res) {
                 var finishPoint = res.geoObjects.get(0).geometry.getCoordinates();
 
-                // Remove previous route if exists
-                if (multiRoute) {
-                    myMap.geoObjects.remove(multiRoute);
-                }
-
-                // Create a multi-route object
-                multiRoute = new ymaps.multiRouter.MultiRoute({
-                    referencePoints: [
-                        startPoint,
-                        finishPoint
-                    ],
-                    params: {
-                        routingMode: 'auto',
-                        avoidTrafficJams: true
-                    }
-                }, {
-                    boundsAutoApply: true
-                });
-
-                // Add the multi-route to the map
-                myMap.geoObjects.add(multiRoute);
-
-                // Display route information
-                multiRoute.model.events.add('requestsuccess', function() {
-                    var activeRoute = multiRoute.getActiveRoute();
-                    if (activeRoute) {
-                        var length = activeRoute.properties.get("distance").text;
-                        var duration = activeRoute.properties.get("duration").text;
-                        alert('Расстояние: ' + length + '\nВремя: ' + duration);
-                    }
-                });
+                // Build route using Yandex Routing API
+                buildRouteWithAPI(startPoint, finishPoint);
             });
         });
     };
+
+    function buildRouteWithAPI(startPoint, finishPoint) {
+        var startCoords = startPoint.join(',');
+        var finishCoords = finishPoint.join(',');
+
+        var apiKey = 'f174ff47-41d0-459b-8b1f-16f0bc134030';  // Replace with your actual API key
+        var url = `https://api.routing.yandex.net/v2/route?waypoints=${startCoords}|${finishCoords}&apikey=${apiKey}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                var route = data.routes[0];
+                var distance = route.legs.reduce((acc, leg) => acc + leg.length.value, 0) / 1000;  // distance in km
+                var duration = route.legs.reduce((acc, leg) => acc + leg.duration_in_traffic.value, 0) / 60;  // duration in minutes
+
+                alert(`Расстояние: ${distance.toFixed(2)} км\nВремя в пути: ${Math.round(duration)} минут`);
+
+                // Draw the route on the map
+                var coords = route.legs.flatMap(leg => leg.steps.map(step => step.start_location));
+
+                var polyline = new ymaps.Polyline(coords, {}, {
+                    strokeColor: "#0000FF",
+                    strokeWidth: 4,
+                    strokeOpacity: 0.5
+                });
+
+                myMap.geoObjects.add(polyline);
+                myMap.setBounds(polyline.geometry.getBounds());
+            })
+            .catch(error => console.error('Ошибка:', error));
+    }
 }
